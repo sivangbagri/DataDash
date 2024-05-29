2348ccb7fea7987d3c87ad4600fba93d901b301c
Shivang <shivangtips@gmail.com>
2024-05-29 15:33:32 +0530

    Fixed dates in dummy , data display on order, employees and customer poge

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Page,
+  Inject,
+  Search,
+  Toolbar
+} from "@syncfusion/ej2-react-grids";
 
+import { employeesData, contextMenuItems, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
 export default function Employees() {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        allowSorting
+        toolbar={['Search']}
+        width="full"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}></ColumnDirective>
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Page, Search,Toolbar]} />
+      </GridComponent>
     </div>
-  )
+  );
 }
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
 
+import { ordersData, contextMenuItems, ordersGrid } from "../data/dummy";
+import { Header } from "../components";
 export default function Orders() {
   return (
-    <div>
-      
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Orders" />
+      <GridComponent id="gridcomp" dataSource={ordersData} 
+          allowPaging
+          allowSorting
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item}></ColumnDirective>
+          ))}
+        </ColumnsDirective>
+        <Inject
+          services={[Resize, Sort,ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit]}
+        />
+      </GridComponent>
     </div>
-  )
+  );
 }
